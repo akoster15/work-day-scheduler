65944ddc480fc7a2c429a4bb643d4e9042a377d2
Andrew Koster <andrewkoster95@gmail.com>
2021-06-25 19:36:56 -0400

    finished index.html and script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,53 @@
 var date = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(date);
 
+$(document).ready(function() {
+    // Assigns saveBtn click listener 
+    $(".saveBtn").on("click", function() {
+        //Get nearby value
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        //Saves items in local storage
+        localStorage.setItem(time, text);
+    })
+
+    //Loads data from localStorage
+    $("#hour8 .description").val(localStorage.getItem("#hour8"));
+    $("#hour9 .description").val(localStorage.getItem("#hour9"));
+    $("#hour10 .description").val(localStorage.getItem("#hour10"));
+    $("#hour11 .description").val(localStorage.getItem("#hour11"));
+    $("#hour12 .description").val(localStorage.getItem("#hour12"));
+    $("#hour13 .description").val(localStorage.getItem("#hour13"));
+    $("#hour14 .description").val(localStorage.getItem("#hour14"));
+    $("#hour15 .description").val(localStorage.getItem("#hour15"));
+    $("#hour16 .description").val(localStorage.getItem("#hour16"));
+    $("#hour17 .description").val(localStorage.getItem("#hour17"));
+
+    function timeTracker() {
+        var currentTime = moment().hour();
+
+        //Loop for time blocks
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+            //if-else statements to check the timne
+            if (timeBlock < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            } 
+            else if (timeBlock === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    timeTracker();
+})
